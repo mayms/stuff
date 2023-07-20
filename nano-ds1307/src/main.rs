35f954775f310da3fdc876ef7d87c15b97331f94
Marcus May <mail@maymarcus.de>
2023-07-20 13:54:49 +0200

    adds day, date, month, year and control

--- nano-ds1307/src/main.rs
+++ nano-ds1307/src/main.rs
@@ -75,6 +75,54 @@ impl RawData {
     pub fn hour(&self) -> u8 {
         ((self.minute & 0b0011_0000) >> 4) * 10 + (self.hour & 0b0000_1111)
     }
+
+    pub fn day(&self) -> u8 {
+        self.date & 0b0000_0111
+    }
+
+    pub fn date(&self) -> u8 {
+        ((self.date & 0b0011_0000) >> 4) * 10 + (self.date & 0b0000_1111)
+    }
+
+    pub fn month(&self) -> u8 {
+        ((self.month & 0b0001_0000) >> 4) * 10 + (self.month & 0b0000_1111)
+    }
+
+    pub fn year(&self) -> u8 {
+        ((self.year & 0b1111_0000) >> 4) * 10 + (self.year & 0b0000_1111)
+    }
+
+    /// Bit 7: Output Control (OUT).
+    /// This bit controls the output level of the SQW/OUT pin when the square-wave
+    /// output is disabled.
+    /// If SQWE = 0, the logic level on the SQW/OUT pin is 1 if OUT = 1 and is 0 if OUT = 0.
+    /// On initial application of power to the device, this bit is typically set to a 0.
+    pub fn control_out(&self) -> bool {
+        ((self.control & 0b1000_0000) >> 7) > 0
+    }
+
+    /// Bit 4: Square-Wave Enable (SQWE).
+    /// This bit, when set to logic 1, enables the oscillator output.
+    /// The frequency of the square-wave output depends upon the value of the RS0 and RS1 bits.
+    /// With the square-wave output set to 1Hz, the clock registers update
+    /// on the falling edge of the square wave.
+    /// On initial application of power to the device, this bit is typically set to a 0.
+    pub fn control_sqwe(&self) -> bool {
+        ((self.control & 0b0001_0000) >> 4) > 0
+    }
+
+    /// Bits 1 and 0: Rate Select (RS[1:0]).
+    /// These bits control the frequency of the square-wave output
+    /// when the square- wave output has been enabled.
+    /// The following table lists the square-wave frequencies that can be selected with the RS bits.
+    /// On initial application of power to the device, these bits are typically set to a 1.
+    /// 0 = 1Hz
+    /// 1 = 4.096kHz
+    /// 2 = 8.192kHz
+    /// 3 = 32.768kHz
+    pub fn control_rate_select(&self) -> u8 {
+        self.control & 0b0000_0011
+    }
 }
 
 #[derive(ufmt::derive::uDebug, Debug, Clone, Copy, Eq, PartialEq)]
